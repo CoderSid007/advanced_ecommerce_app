326f80206ec0c7316e1d73d5a596b1877b63944d
CoderSid007 <siddhesdas2020@gmail.com>
2024-07-24 14:55:56 +0530

    added_text_themes

---
+++ lib/utils/theme/custom_themes/text_theme.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+
+class AETextTheme {
+  AETextTheme._();
+
+  // LIGHT THEME CLASS
+
+  static TextTheme LightTextTheme = TextTheme(
+
+      //Headlines described from here
+
+      headlineLarge: const TextStyle().copyWith(
+        fontSize: 32,
+        fontWeight: FontWeight.bold,
+        color: Colors.black,
+      ),
+      headlineMedium: const TextStyle().copyWith(
+        fontSize: 24,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      ),
+      headlineSmall: const TextStyle().copyWith(
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      ),
+
+      //Titles described from here
+
+      titleLarge: const TextStyle().copyWith(
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      ),
+      titleMedium: const TextStyle().copyWith(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        color: Colors.black,
+      ),
+      titleSmall: const TextStyle().copyWith(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+      ),
+
+      //Body described from here
+
+      bodyLarge: const TextStyle().copyWith(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color: Colors.black,
+      ),
+      bodyMedium: const TextStyle().copyWith(
+        fontSize: 14,
+        fontWeight: FontWeight.normal,
+        color: Colors.black,
+      ),
+      bodySmall: const TextStyle().copyWith(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color: Colors.black.withOpacity(0.5),
+      ),
+
+      // Label Described from here
+
+      labelLarge: const TextStyle().copyWith(
+        fontSize: 12,
+        fontWeight: FontWeight.normal,
+        color: Colors.black,
+      ),
+      labelMedium: const TextStyle().copyWith(
+        fontSize: 12,
+        fontWeight: FontWeight.normal,
+        color: Colors.black.withOpacity(0.5),
+      ));
+
+  // DARK THEME CLASS
+
+  static TextTheme DarkTextTheme = TextTheme(
+
+      //Headlines described from here
+
+      headlineLarge: TextStyle().copyWith(
+        fontSize: 32,
+        fontWeight: FontWeight.bold,
+        color: Colors.white,
+      ),
+      headlineMedium: TextStyle().copyWith(
+        fontSize: 24,
+        fontWeight: FontWeight.w600,
+        color: Colors.white,
+      ),
+      headlineSmall: const TextStyle().copyWith(
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      ),
+
+      //Titles described from here
+
+      titleLarge: const TextStyle().copyWith(
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+        color: Colors.black,
+      ),
+      titleMedium: const TextStyle().copyWith(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        color: Colors.black,
+      ),
+      titleSmall: const TextStyle().copyWith(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+      ),
+
+      //Body described from here
+
+      bodyLarge: const TextStyle().copyWith(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color: Colors.black,
+      ),
+      bodyMedium: const TextStyle().copyWith(
+        fontSize: 14,
+        fontWeight: FontWeight.normal,
+        color: Colors.black,
+      ),
+      bodySmall: const TextStyle().copyWith(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        color: Colors.black.withOpacity(0.5),
+      ),
+
+      // Label Described from here
+
+      labelLarge: const TextStyle().copyWith(
+        fontSize: 12,
+        fontWeight: FontWeight.normal,
+        color: Colors.black,
+      ),
+      labelMedium: const TextStyle().copyWith(
+        fontSize: 12,
+        fontWeight: FontWeight.normal,
+        color: Colors.black.withOpacity(0.5),
+      ));
+}
